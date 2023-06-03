096ab8b45e44ea601da5350310c5b9a9db047def
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2023-06-04 05:51:05 +1000

    Add files via upload
    Individual class header files in the .zip

---
+++ ida_funcs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import idaapi
+import idc
+from idaapi import SN_NOWARN, SN_NOCHECK
+import json
+
+path = idaapi.ask_file(False, '*.json', 'script.json')
+json_data = json.loads(open(path, 'rb').read().decode('utf-8'))
+
+imageBase = idaapi.get_imagebase()
+
+for func_entry in json_data['Functions']:
+    name = func_entry['Name']
+    addr = imageBase+func_entry['Address']
+    ret = idc.set_name(addr, name, SN_NOWARN | SN_NOCHECK)
+    if ret == 0:
+       new_name = name+'_'+str(addr)
+       ret = idc.set_name(addr, new_name, SN_NOWARN | SN_NOCHECK)

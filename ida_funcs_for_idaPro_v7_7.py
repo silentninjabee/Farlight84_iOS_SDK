ca10073522d84794d54c8bf2adff041a15d66418
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2023-08-02 22:31:39 +1000

    ida_funcs script for IDAPro v7.7

---
+++ ida_funcs_for_idaPro_v7_7.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# IDA Pro version 7.7
+import json
+import idaapi
+import idc
+
+imageBase = idaapi.get_imagebase()
+
+def get_addr(addr):
+    return imageBase + addr
+
+def set_name(addr, name):
+    ret = idc.set_name(addr, name, idc.SN_NOWARN | idc.SN_NOCHECK)
+    if ret == 0:
+        new_name = name + '_' + str(addr)
+        ret = idc.set_name(addr, new_name, idc.SN_NOWARN | idc.SN_NOCHECK)
+
+def make_function(start, end):
+    next_func = idc.get_next_func(start)
+    if next_func < end:
+        end = next_func
+    if idc.get_func_attr(start, idc.FUNCATTR_START) == start:
+        ida_funcs.del_func(start)
+    ida_funcs.add_func(start, end)
+
+path = idaapi.ask_file(False, '*.json', 'script.json')
+data = json.loads(open(path, 'rb').read().decode('utf-8'))
+
+for function in data["Functions"]:
+    addr = get_addr(function["Address"])
+    name = function["Name"]
+    set_name(addr, name)
+
+print('Script finished!')
